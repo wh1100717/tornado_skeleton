28299a75e368f653e518b25365bd2a2df897fe13
Wang Hao <eric.3126@hotmail.com>
2014-02-23 20:52:07 +0800

    Add: FriendController && GroupController

--- handler.py
+++ handler.py
@@ -5,6 +5,10 @@ import tornado.web
 from util import TemplateUtil
 from controller import UserController
 from controller import DeviceController
+from controller import GroupController
+from controller import FriendController
+
+defaultHandler = tornado.web.RequestHandler
 
 class SpecificHandler(TemplateUtil.DefaultHandler):
     def get(self):
@@ -14,9 +18,8 @@ class MainHandler(TemplateUtil.DefaultHandler):
     def get(self, filename):
         self.write(TemplateUtil.render_template(filename))
 
-#####################User Hanlders###############
-
-class UserRegisterHandler(tornado.web.RequestHandler):
+#####################User Handlers###############
+class UserRegisterHandler(defaultHandler):
 	def get(self):
 		username = self.get_argument('username')
 		password = self.get_argument('password')
@@ -24,16 +27,53 @@ class UserRegisterHandler(tornado.web.RequestHandler):
 		self.write("username: " + username + " | password: " + password + "<br>")
 		self.write(str(res))
 
-class UserLoginHandler(tornado.web.RequestHandler):
+class UserLoginHandler(defaultHandler):
 	def get(self):
 		username = self.get_argument('username')
 		password = self.get_argument('password')
 		res = UserController.login(username, password)
 		self.write(res)
 
-#####################Device Hanlders###############
+class UserUpdateInfoHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		nick_name = self.get_argument('nick_name')
+		sex = self.get_argument('sex')
 
-class DeviceEnrollHandler(tornado.web.RequestHandler):
+class UserUpdateAvatarHandler(defaultHandler):
+	def get(self):
+		pass
+
+class UserUpdateEmailHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		email = self.get_argument('email')
+		res = UserController.update_email(uid, email)
+		self.write(res)
+
+class UserVerifyEmailHandler(defaultHandler):
+	def get(self):
+		verify_code = self.get_argument('verify_code')
+		res = UserController.verify_email(verify_code)
+		self.write(res)
+
+class UserUpdatePhoneNumberHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		phone_number = self.get_argument('phone_number')
+		verify_code = self.get_argument('verify_code')
+		res = UserController.update_phone_number(uid, phone_number, verify_code)
+		self.write(res)
+
+class UserSendPhoneVerifyCodeHandler(defaultHandler):
+	def get(self):
+		phone_umber = self.get_argument('phone_umber')
+		res = UserController.send_phone_verify_code(phone_umber)
+		self.write(res)
+
+#####################Device Handlers###############
+
+class DeviceEnrollHandler(defaultHandler):
 	def get(self):
 		uid = self.get_argument('uid')
 		imei = self.get_argument('imei')
@@ -41,25 +81,112 @@ class DeviceEnrollHandler(tornado.web.RequestHandler):
 		res = DeviceController.enroll(uid, imei, phone_type)
 		self.write(res)
 
-class DeviceUnenrollHandler(tornado.web.RequestHandler):
+class DeviceUnenrollHandler(defaultHandler):
 	def get(self):
 		uid = self.get_argument('uid')
 		cid = self.get_argument('cid')
 		res = DeviceController.unenroll(uid, cid)
 		self.write(res)
 
-class DeviceSyncHandler(tornado.web.RequestHandler):
+class DeviceSyncHandler(defaultHandler):
 	def get(self):
 		res = DeviceController.sync()
 		self.write(res)
 
-class DeviceListHandler(tornado.web.RequestHandler):
+class DeviceListHandler(defaultHandler):
 	def get(self):
 		uid = self.get_argument('uid')
 		res = DeviceController.list(uid)
 		self.write(res)
 
-#####################Group Hanlders###################
+#####################Group Handlers###################
+class GroupCreateHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		group_name = self.get_argument('group_name')
+		group_type = self.get_argument('group_type')
+		res = GroupController.create(uid, group_name, group_type)
+		self.write(res)
+
+class GroupDeleteHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		res = GroupController.delete(uid, gid)
+		self.write(res)
+
+class GroupInviteMembersHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		fid_list = self.get_argument('fid_list').split(',')
+		res = GroupController.invite_members(uid, gid, fid_list)
+		self.write(res)
+
+class GroupRemoveMembersHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		fid_list = self.get_argument('fid_list').split(',')
+		res = GroupController.delete_members(uid, gid, fid_list)
+		self.write(res)
+
+class GroupSetAdminHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		fid = self.get_argument('fid')
+		res = GroupController.set_admin(uid, gid, fid)
+		self.write(res)
+
+class GroupRemoveAdminHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		fid = self.get_argument('fid')
+		res = GroupController.remove_admin(uid, gid, fid)
+		self.write(res)
+
+class GroupMemberListHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		res = GroupController.member_list(uid, gid)
+		self.write(res)
+
+class GroupMessageBroadcastHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		gid = self.get_argument('gid')
+		msg_type = self.get_argument('msg_type')
+		msg_content = self.get_argument('msg_content')
+		res = GroupController.message_broadcast(uid, gid, msg_type, msg_content)
+		self.write(res)
+
+#####################Friend Handlers###################
+class FriendAddHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		fid = self.get_argument('fid')
+		res = FriendController.add(uid, fid)
+		self.write(res)
+
+class FriendAcceptHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		fid = self.get_argument('fid')
+		res = FriendController.accept(uid, fid)
+		self.write(res)
+
+class FriendRemoveHandler(defaultHandler):
+	def get(self):
+		uid = self.get_argument('uid')
+		fid = self.get_argument('fid')
+		res = FriendController.remove(uid, fid)
+		self.write(res)
+
+
+
 
 
 handlers = [
@@ -72,6 +199,8 @@ handlers = [
 	(r"/device/sync", DeviceSyncHandler), #设备同步
 	(r"/device/list", DeviceListHandler), #获取登记设备列表
 	#Group Handlers
+	(r"/group/create",GroupCreateHandler),
+	#Friend Handlers
 
 	#Request Sample
     (r"/specific_request", SpecificHandler),
